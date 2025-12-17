e6d2bfa72b2acf9622a280b46cd9162eae2026f3
Appa1990 <appa1990@web.de>
2025-12-17 10:22:42 +0100

    Delete scum3 directory

--- scum3/config/topgames.php
+++
@@ -1,9 +0,0 @@
-<?php
-// /config/topgames.php
-declare(strict_types=1);
-
-const TOPGAMES_API_BASE   = 'https://api.top-games.net/v1';
-const TOPGAMES_SERVER_TOKEN = 'servertokenfrompage'; // aus dem Top-Games Admin
-
-// Gutscheine pro erfolgreichem Vote-Claim
-const VOTE_VOUCHERS_PER_VOTE = 1;
--- scum3/includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Basis-URL deiner Seite (OHNE Slash am Ende)
-define('APP_BASE_URL', 'https://deine_URL_zum_verzeichnis.de');
-
-// Wohin nach erfolgreichem Login?
-define('APP_LOGIN_REDIRECT', APP_BASE_URL . '/index.php?page=home');
-
-// OpenID Endpoint
-define('STEAM_OPENID', 'https://steamcommunity.com/openid/login');
